d63a0d3461b95c5ea442b2e49405d335b8cf755d
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-04 11:20:25 +0100

    header complete with styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,10 +3,10 @@ import dog_logo from "../images/dog-logo.svg"
 
 export default function Header():JSX.Element {
     return (
-        <section id="header">
-        <img src={dog_logo} alt="logo of dog"/>
-        <h1> Pupvote </h1>
-        <h2> Vote for a good boy today </h2>
+        <section className="header">
+        <img className="logo" src={dog_logo} alt="logo of dog"/>
+        <h1 className="header--title"> Pupvote </h1>
+        <h2 className="header--description"> Vote for a good boy today </h2>
         </section>
     )
 }
