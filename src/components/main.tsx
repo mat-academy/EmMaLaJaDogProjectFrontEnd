68eb35ad62de329a2202109ad3d51d3fabe36ec6
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-04 10:33:47 +0100

    added 6 new component for react app

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,3 +1,10 @@
 //top3breed
+    //separate component:
+    //title, flexbox with 3 dogs
+
 //voteforgoodboy
+    //vote text, flexbox with two random dogs
+
 //top10list
+    //endless scroll component 
+    //goes through the ten topdogs
