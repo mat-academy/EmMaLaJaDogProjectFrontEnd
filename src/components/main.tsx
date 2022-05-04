2a0036fb14f729ad745dd69ea8642b8a1b2fa837
emanfalola9998 <emmanuelfalola9998@gmail.com>
2022-05-04 14:46:34 +0100

    correctd code

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,6 @@
 import TopThree from "./topthree";
 import Vote from "./vote";
 import TopTen from "./topten";
-import { useEffect, useState } from "react";
-import axios from "axios";
 
 export default function Main(): JSX.Element {
   return (
