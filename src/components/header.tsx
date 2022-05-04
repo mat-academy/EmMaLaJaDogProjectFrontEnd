924b4b15c79b1bb92d32cb9376f5d05c896b7541
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-04 11:03:50 +0100

    added elements to header/unstyled

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
 // to include logo, header text, vote text
+import dog_logo from "../images/dog-logo.svg"
+
 export default function Header():JSX.Element {
     return (
         <section id="header">
-            <></>
-            <></>
+        <img src={dog_logo} alt="logo of dog"/>
+        <h1> Pupvote </h1>
+        <h2> Vote for a good boy today </h2>
         </section>
     )
 }
