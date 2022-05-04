5ac17e137981617a3dd93fc0e8d3052adea28898
Matty B <mb10@Mats-MacBook.cable.virginm.net>
2022-05-04 10:53:08 +0100

    added image resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { greet } from "./utils/greet";
 import Header from "./components/header";
 import Main from "./components/main"
 import Footer from "./components/footer";
 
-
 function App(): JSX.Element {
   return <>
-  <h1>{greet("Welcome to Pupvote")}</h1>;
   <Header/>;
   <Main/>;
   <Footer/>;
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,9 @@
 // to include logo, header text, vote text
 export default function Header():JSX.Element {
     return (
-        <>"Value"</>
+        <section id="header">
+            <></>
+            <></>
+        </section>
     )
 }
