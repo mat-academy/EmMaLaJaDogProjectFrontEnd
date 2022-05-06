1fface1664b9d25a874e027e09aae014b269c83c
laura J <laura@academy.com>
2022-05-06 13:23:25 +0100

    updated structure of top 3 to add space for fetched pictures

--- src/components/topthree.tsx
+++ src/components/topthree.tsx
@@ -2,6 +2,7 @@
 //separate component:
 //title, flexbox with 3 dogs
 import { readNameFormatter } from "../utils/readNameFormatter";
+import fakeDog from "../images/FakeDog.png";
 
 interface TopDogsInterface {
   breed_name: string;
@@ -24,41 +25,58 @@ export default function TopThree(props: TopThreeProps): JSX.Element {
   const voteMax = top3Dogs[0].count;
   const dog1Score = 10;
 
-  const dog2Score = Math.round((top3Dogs[1].count/voteMax) * 10 )
-  const dog3Score = Math.round((top3Dogs[2].count/voteMax) * 10 )
+  const dog2Score = Math.round((top3Dogs[1].count / voteMax) * 10);
+  const dog3Score = Math.round((top3Dogs[2].count / voteMax) * 10);
 
-
-  function getPawsArray(score: number): string[]{ 
+  function getPawsArray(score: number): string[] {
     let count = 0;
-    const arr = []
-    while (count < score){
-      arr.push("🐾")
-      count ++
+    const arr = [];
+    while (count < score) {
+      arr.push("🐾");
+      count++;
     }
-    return arr
+    return arr;
   }
 
-  const dogPawArray1 = getPawsArray(dog1Score)
-  const dogPawArray2 = getPawsArray(dog2Score)
-  const dogPawArray3 = getPawsArray(dog3Score)
+  const dogPawArray1 = getPawsArray(dog1Score);
+  const dogPawArray2 = getPawsArray(dog2Score);
+  const dogPawArray3 = getPawsArray(dog3Score);
   return (
     <>
       <h1 className="title">TOP BREEDS</h1>
       <div className="top3">
         <div className="top3--element">
-          <h3>{readNameFormatter(top3Dogs[0].breed_name)}</h3>
+          <div className="top3--profile">
+          <div className="top3--info">
+            <h3>{readNameFormatter(top3Dogs[0].breed_name)}</h3>
+            <h3>⭐ {top3Dogs[0].count}</h3>
+          </div>
+          <img className="dogpic" src={fakeDog} alt="dog" />
+          </div>
+          <h3 className="voteheader">VOTE RATIO:</h3>
           <h2 className="pawbar">{dogPawArray1}</h2>
-          <h3>⭐ {top3Dogs[0].count}</h3>
         </div>
         <div className="top3--element">
-          <h3>{readNameFormatter(top3Dogs[1].breed_name)}</h3>
+        <div className="top3--profile">
+          <div className="top3--info">
+            <h3>{readNameFormatter(top3Dogs[1].breed_name)}</h3>
+            <h3>⭐ {top3Dogs[1].count}</h3>
+          </div>
+          <img className="dogpic" src={fakeDog}  alt="dog" />
+          </div>
+          <h3 className="voteheader">VOTE RATIO:</h3>
           <h2 className="pawbar">{dogPawArray2}</h2>
-          <h3>⭐ {top3Dogs[1].count}</h3>
         </div>
         <div className="top3--element">
-          <h3>{readNameFormatter(top3Dogs[2].breed_name)}</h3>
+        <div className="top3--profile">
+          <div className="top3--info">
+            <h3>{readNameFormatter(top3Dogs[2].breed_name)}</h3>
+            <h3>⭐ {top3Dogs[2].count}</h3>
+          </div>
+          <img className="dogpic" src={fakeDog}  alt="dog" />
+          </div>
+          <h3 className="voteheader">VOTE RATIO:</h3>
           <h2 className="pawbar">{dogPawArray3}</h2>
-          <h3>⭐ {top3Dogs[2].count}</h3>
         </div>
       </div>
       <div className="top3--button">
